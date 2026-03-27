cae1b7033202edc954194fa1cac1f8cc0cb03190
Wiebe-Jan <wiebejan_91@hotmail.com>
2026-03-27 09:44:31 +0100

    add hook for manual update logging hook "upgrader_process_complete"

--- duracom-maintenance-worker.php
+++ duracom-maintenance-worker.php
@@ -3,13 +3,15 @@
 Plugin Name: Duracom maintenance worker
 Plugin URI: https://duracom.nl/
 Description: Call maintenance hooks in duracom backoffice
-Version: 1.0
+Version: 1.1
 Author: Wiebe-Jan Valkema
 Author URI: https://valkemedia.nl/
 Gemaakt door: WJ Valkema
 License: GPLv2 or later
 Text Domain: duracom
 */
+
+const DURACOM_TOKEN = 'lacFB%@6qH42#dK#2mrnQopf!BX67&#m';
 
 if (!defined('ABSPATH')) {
     exit;
@@ -27,23 +29,45 @@ class duracom_maintenance_worker
          *
          * @since  3.8.0
          */
-        add_action('automatic_updates_complete', function($results ){
+        add_action('automatic_updates_complete', function ($results) {
             $results = json_decode(json_encode($results), true);
-
-            $auth_key = md5('lacFB%@6qH42#dK#2mrnQopf!BX67&#m');
-            $hook = 'https://bo.duracom.nl/hook/domain/updated';
-            $url = sprintf($hook . '?key=%s&domain_url=%s&msg=%s', $auth_key, get_site_url(), serialize($results));
-            
-            $response = wp_remote_get($url, [
-                'headers' => [
-                    'Accept-Language' => 'en-US'
-                ],
-                'timeout' => 30,
-                'redirection' => 5,
-                'httpversion' => '1.1',
-                'sslverify' => false
-            ]);
+            $this->callback(serialize($results));
         });
+
+        /**
+         * hook for manual updates callbacks
+         */
+        add_action('upgrader_process_complete', function ($upgrader_object, $options) {
+//            if ( $options['action'] !== 'update' || $options['type'] !== 'plugin' ) {
+//                return;
+//            }
+
+            $results = json_decode(json_encode($options), true);
+            $this->callback(serialize($results));
+        }, 10, 2);
+    }
+
+    /**
+     * @param $data
+     *
+     * @return mixed
+     */
+    private function callback($data)
+    {
+        return wp_remote_get(sprintf(
+            'https://bo.duracom.nl/hook/domain/updated?key=%s&domain_url=%s&msg=%s',
+            md5(DURACOM_TOKEN),
+            get_site_url(),
+            serialize($data)
+        ), [
+            'headers' => [
+                'Accept-Language' => 'en-US'
+            ],
+            'timeout' => 30,
+            'redirection' => 5,
+            'httpversion' => '1.1',
+            'sslverify' => false
+        ]);
     }
 }
 
